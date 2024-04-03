1c5701dffc1a543f7a2e91635b693a93480ff5ae
kpayyam1998 <karuppasamy12041998@gmail.com>
2024-04-03 22:27:27 +0530

    logger added

--- README.md
+++ README.md
@@ -8,3 +8,12 @@
     * pip install -r requirements.txt 
     * this will automaticlly call setup.py and install all files b
     * once installed one folder which means packages all installed
+
+# template.py
+
+    * run template file to create automate project structure
+
+# Code
+
+    * exception.py
+    * logger.py
---
+++ src/logger.py
@@ -0,0 +1,28 @@
+""""
+    Logger which is nothing but whenever excuting our program it will track everything 
+    Even custom excepton error also we can log
+
+    If want to learn more gothrouth document
+
+"""
+
+
+import os
+import logging
+
+from datetime import datetime
+
+LOGFILE=f"{datetime.now().strftime('%m_%d_%y_%H_%M_%S')}.log" # Getting current date time with extension  .log filename
+log_path=os.path.join(os.getcwd(),"logs",LOGFILE) # combine with path and file name
+os.makedirs(log_path,exist_ok=True) # crating directory
+
+
+LOGS_FILE_PATH=os.path.join(log_path,LOGFILE) # full path and file name
+
+logging.basicConfig(
+    filename=LOGS_FILE_PATH,
+
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s -%(message)s",
+    level=logging.INFO # this line going to write my exitre msg
+
+)
