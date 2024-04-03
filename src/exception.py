ea3222aaacb5e5b04bf0e285d33f7b404fe83292
kpayyam1998 <karuppasamy12041998@gmail.com>
2024-04-03 22:11:22 +0530

    Exception added

---
+++ src/exception.py
@@ -0,0 +1,21 @@
+import sys
+
+def error_message_details(error,error_details:sys):
+
+    """ read the exception documention for own  custom exceptions """
+    _,_,exc_tb=error_details.exc_info() # 3rd parameter will give every thing like all error details
+    file_name=exc_tb.tb_frame.f_code.co_filename # this will hep to get the file name 
+    error_message="error occurend in python script [{0}] line number [{1}] error message [{2}]".format(
+        file_name,exc_tb.tb_lineno,str(error)
+
+    )
+    return error_message
+
+
+class CustomException(Exception):
+    def __init__(self, error_message,error_detail:sys):
+        super.__init__(error_message)
+        self.error_message=error_message_details(error_message,error_detail=error_detail)
+
+    def __str__(self) :
+        return self.error_message
